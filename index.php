e7572741d6c13a5a919ac8eda4b8eeb845effd4c
askrosid <ask.rosid@gmail.com>
2023-11-26 20:07:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
    <div class="max-w-7xl mx-auto px-3 py-8 sm:py-9 md:py-11 xl:py-14">
-      <h1 class="text-2xl font-semibold text-black-200 text-center">Maaf, sebagian pages pada web www.ruanggradasi.com masih dalam tahap <span class="text-green-100">development</span></h1>
+      <h1 class="text-2xl font-semibold text-black-200 text-center">Maaf, sebagian pages pada web www.ruanggradasi.com masih dalam tahap <span class="text-green-100">development!</span></h1>
    </div>
 <?php get_footer(); ?>
